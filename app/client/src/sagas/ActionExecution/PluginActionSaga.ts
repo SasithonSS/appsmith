cb6fa7c0173c48c048de8f268aaece23df6d83bd
Rajat Agrawal <rajat@appsmith.com>
2024-03-05 15:56:58 +0530

    run setupUpdateTree and evals in eval trigger only on not first page load instances

--- app/client/src/sagas/ActionExecution/PluginActionSaga.ts
+++ app/client/src/sagas/ActionExecution/PluginActionSaga.ts
@@ -1094,6 +1094,7 @@ function* executeOnPageLoadJSAction(pageAction: PageAction) {
       action: jsAction,
       collection,
       isExecuteJSFunc: true,
+      onPageLoad: true,
     };
 
     yield call(handleExecuteJSFunctionSaga, data);
